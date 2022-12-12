bb04d6b7bf2e7f41f90526ef90f4ae157dbe8e84
Kevin Harrington <kharrington@softroboticsinc.com>
2022-12-12 13:55:29 -0500

    copy file content

---
+++ walking.groovy
@@ -0,0 +1,26 @@
+
+import java.time.Duration;
+
+import java.util.ArrayList;
+
+import javafx.application.Platform;
+
+import org.reactfx.util.FxTimer;
+
+import com.neuronrobotics.sdk.addons.kinematics.DHParameterKinematics;
+import com.neuronrobotics.sdk.addons.kinematics.MobileBase;
+import com.neuronrobotics.sdk.addons.kinematics.math.RotationNR;
+import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR;
+import com.neuronrobotics.sdk.util.ThreadUtil;
+import com.neuronrobotics.sdk.addons.kinematics.IDriveEngine;
+
+
+return new IDriveEngine() {
+
+	@Override
+	public void DriveArc(MobileBase arg0, TransformNR arg1, double arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}           
